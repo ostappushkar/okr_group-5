fe677bf432d75741dd096c19eb2afbf06b947f10
Ostap Pushkar <30481088+ostappushkar@users.noreply.github.com>
2018-04-25 10:44:18 +0300

    Add files via upload

---
+++ BeaconAPI/app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require("mysql");
+var indexRouter = require('./routes/index');
+var beacon = require('./routes/beacon');
+
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+	global.connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'Ostap',
+		password : '12345',
+		database : 'mybeacon'
+	});
+	connection.connect();
+	next();
+});
+
+app.use('/', indexRouter);
+app.use('/beacon', beacon);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
